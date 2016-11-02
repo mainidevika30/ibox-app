e8b60ecb44e762b381c6c6fe6393cb92b28adc98
mainidevika30 <devikaersagi@gmail.com>
2016-11-01 19:32:20 -0700

    integration testing update

--- src/test/java/edu/csupomona/cs585/ibox/IntGoogleFile.java
+++ src/test/java/edu/csupomona/cs585/ibox/IntGoogleFile.java
@@ -2,6 +2,7 @@ package edu.csupomona.cs585.ibox;
 
 import static org.mockito.Mockito.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -11,21 +12,30 @@ import edu.csupomona.cs585.ibox.sync.GoogleDriveFileSyncManager;
 import edu.csupomona.cs585.ibox.sync.GoogleDriveServiceProvider;
 import junit.framework.TestCase;
 
-public class IntGoogleFile {
+public class IntGoogleFile extends TestCase{
 	GoogleDriveFileSyncManager obj = new GoogleDriveFileSyncManager(
 			GoogleDriveServiceProvider.get().getGoogleDriveClient());
-	java.io.File localFile = new java.io.File("/Users/Devika/Desktop/devika.rtf");
+	File localFile = new File("/Users/Devika/Desktop/myFileDevika.rtf");
+	String fileName = "myFileDevika.rtf";
 
-	@Test
-	public void FileAddTest() throws IOException {
+	public void testAadd() throws IOException {
 		obj.addFile(localFile);
+		String id = obj.getFileId(fileName);
+		assertNotNull(id);
+		System.out.println("File added");
 	}
-	@Test
-	public void FileUpdateTest() throws Exception {
-	obj.updateFile(localFile);
-}
-	@Test
-	public void FileDeleteTest() throws Exception {
-	obj.deleteFile(localFile);
-}
+
+	public void testBupdate() throws Exception {
+		obj.updateFile(localFile);
+		String id = obj.getFileId(fileName);
+		assertNotNull(id);
+		System.out.println("File Updated");
+	}
+
+	public void testCdelete() throws Exception {
+		obj.deleteFile(localFile);
+		String id = obj.getFileId(fileName);
+		assertNull(id);
+		System.out.println("File Deleted");
+	}
 }
